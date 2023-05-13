0dc553706a5222e72630cc16d48493016297a5b8
ryuu <ryuu@gmx.de>
2023-05-13 11:45:57 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import ProductView from "./features/ProductView/ProductView";
-import React, { useMemo } from 'react';
-const Context = React.createContext({
-  name: 'Default',
-});
 
 function App() {
-    const contextValue = useMemo(
-        () => ({
-            name: 'Ant Design',
-        }),
-        [],
-    );
-  return (<>
-      <Context.Provider value={contextValue}>
-          <ProductView />
-      </Context.Provider>
-  </>
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
